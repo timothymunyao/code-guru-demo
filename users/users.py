c7584e4b5297a83ad15f2c9a4b01841c32c04def
Dickson <kibedickson3@gmail.com>
2024-02-12 12:34:21 +0300

    finalize

--- users/users.py
+++ users/users.py
@@ -1,37 +1,37 @@
 import os
 import json
-# import mysql.connector
+import pymysql
 
 
 def handler(event, context):
-    # db_host = os.environ.get("DB_HOST")
-    # db_user = "test"
-    # db_password = "Qwerty123!"
-    # db_name = "test"
-    #
-    # # Connect to the database
-    # connection = mysql.connector.connect(
-    #     host=db_host,
-    #     user=db_user,
-    #     password=db_password,
-    #     database=db_name
-    # )
-    #
-    # # Execute your query
-    # cursor = connection.cursor()
-    # cursor.execute("SELECT * FROM users")
-    # data = cursor.fetchall()
-    #
-    # # Close the connection
-    # connection.close()
-    #
-    # # Convert data to JSON
-    # json_data = json.dumps(data)
+    db_host = os.environ.get("DB_HOST")
+    db_user = "test"
+    db_password = "Qwerty123!"
+    db_name = "test"
+
+    # Connect to the database
+    connection = pymysql.connect(
+        host=db_host,
+        user=db_user,
+        password=db_password,
+        database=db_name
+    )
+
+    # Execute your query
+    cursor = connection.cursor()
+    cursor.execute("SELECT * FROM users")
+    data = cursor.fetchall()
+
+    # Close the connection
+    connection.close()
+
+    # Convert data to JSON
+    json_data = json.dumps(data)
 
     # Return the JSON data
     return {
         "statusCode": 200,
-        "body": {'test': 'test'},
+        "body": json_data,
         "headers": {
             "Content-Type": "application/json"
         }
